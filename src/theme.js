ed46c59c6bf864700571f70ab6650905e7362aa7
Ion Batîr <ionbatir@gmail.com>
2019-09-16 20:48:53 +0300

    Add theme file

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+
+export const SPACING = {
+  EXTRA_SMALL: 4,
+  SMALL: 8,
+  MEDIUM: 16,
+  LARGE: 24,
+  EXTRA_LARGE: 32
+};
+
+export const COLORS = {
+  BACKGROUND: "#fff"
+};
