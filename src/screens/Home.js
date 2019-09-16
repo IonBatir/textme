711ed6e6192d1716b7f1b67af5506c50ca3730dc
Ion Batîr <ionbatir@gmail.com>
2019-09-16 21:07:33 +0300

    Update home style

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={styles.container}>
       <Text>Home</Text>
     </View>
   );
