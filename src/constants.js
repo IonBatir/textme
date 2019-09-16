ec33f540eac88faa0e6f4ea471b9eab813644b21
Ion Batîr <ionbatir@gmail.com>
2019-09-16 21:19:43 +0300

    Create bottom navigation

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 
 export const APP_NAME = "TextMe";
+
+// SCREENS
+export const HOME_SCREEN = "Home";
+export const GROUPS_SCREEN = "Groups";
+export const SETTINGS_SCREEN = "Settings";
