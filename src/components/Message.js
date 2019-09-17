faf34ff851769ba07de41f23a5c9d6e8318516bc
Ion Batîr <ionbatir@gmail.com>
2019-09-17 21:39:15 +0300

    Add Message component

---
+++ src/components/Message.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { StyleSheet, View, Image, Text } from "react-native";
+import { SPACING, FONT_SIZES } from "../theme";
+
+const styles = StyleSheet.create({
+  container: {
+    height: 76,
+    margin: SPACING.MEDIUM
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  content: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  textContent: {
+    paddingLeft: SPACING.SMALL
+  },
+  nameText: {
+    fontFamily: "Nunito-SemiBold",
+    fontSize: FONT_SIZES.EXTRA_LARGE,
+    color: "#3C444C"
+  },
+  messageText: {
+    fontFamily: "Nunito-Light",
+    fontSize: FONT_SIZES.SMALL,
+    color: "#8B8B8B"
+  },
+  verticalLine: {
+    height: 39,
+    borderLeftWidth: 0.5,
+    borderLeftColor: "#D5D5D5",
+    paddingRight: SPACING.EXTRA_SMALL
+  },
+  dateText: {
+    fontFamily: "Nunito-LightItalic",
+    fontSize: FONT_SIZES.EXTRA_SMALL,
+    color: "#E9A823",
+    textAlign: "right",
+    paddingBottom: SPACING.EXTRA_SMALL
+  },
+  timeText: {
+    fontFamily: "Nunito-LightItalic",
+    fontSize: FONT_SIZES.EXTRA_SMALL,
+    color: "#E9A823",
+    textAlign: "right"
+  }
+});
+
+const MAX_TEXT_LENGTH = 25;
+
+export default function Message({ avatar, name, text, date, time }) {
+  const message =
+    text.length > MAX_TEXT_LENGTH
+      ? text.substring(0, MAX_TEXT_LENGTH - 3).concat("...")
+      : text;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <View style={styles.content}>
+          <Image source={avatar} />
+          <View style={styles.textContent}>
+            <Text style={styles.nameText}>{name}</Text>
+            <Text style={styles.messageText}>{message}</Text>
+          </View>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.verticalLine} />
+          <View>
+            <Text style={styles.dateText}>{date}</Text>
+            <Text style={styles.timeText}>{time}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 
 export { default as Header } from "./Header";
+export { default as Message } from "./Message";
