52d65a7f03f3e9ea47d8b0acef1433230c845fd0
Ion Batîr <ionbatir@gmail.com>
2019-10-04 21:55:14 +0300

    Add TextField component

---
+++ src/components/TextField.js
@@ -0,0 +1,52 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { StyleSheet, Platform, View, TextInput, Text } from "react-native";
+import { SPACING, FONT_FAMILY, FONT_SIZE, COLOR } from "../theme";
+
+const styles = StyleSheet.create({
+  input: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    marginBottom: SPACING.LARGE,
+    paddingBottom: Platform.OS === "ios" ? SPACING.SMALL : 0
+  },
+  inputText: {
+    flex: 1,
+    fontFamily: FONT_FAMILY.NUNITO_REGULAR,
+    fontSize: FONT_SIZE.MEDIUM,
+    color: "rgba(11, 13, 15, 0.5)"
+  },
+  errorText: {
+    fontFamily: FONT_FAMILY.NUNITO_LIGHT_ITALIC,
+    fontSize: FONT_SIZE.EXTRA_EXTRA_SMALL,
+    color: COLOR.RED
+  }
+});
+
+export default function TextField({
+  inputRef,
+  onChangeText,
+  value,
+  error,
+  ...other
+}) {
+  return (
+    <View
+      style={[
+        styles.input,
+        { borderBottomColor: error ? COLOR.RED : "rgba(112, 112, 112, 0.5)" }
+      ]}
+    >
+      <TextInput
+        ref={inputRef}
+        style={styles.inputText}
+        onChangeText={onChangeText}
+        value={value}
+        {...other}
+      />
+      {error && <Text style={styles.errorText}>{error}</Text>}
+    </View>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Message } from "./Message";
 export { default as Contact } from "./Contact";
 export { default as ChatMessage } from "./ChatMessage";
 export { default as Spinner } from "./Spinner";
+export { default as TextField } from "./TextField";
