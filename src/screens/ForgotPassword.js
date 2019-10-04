8511440bd2e2fea49425a5f4e20569f30e886360
Ion Batîr <ionbatir@gmail.com>
2019-10-04 23:40:21 +0300

    ForgotPassword screen

--- src/screens/ForgotPassword.js
+++ src/screens/ForgotPassword.js
@@ -1,14 +1,9 @@
-import React, { useState } from "react";
-import {
-  Platform,
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity
-} from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, View, Text, TouchableOpacity, Alert } from "react-native";
+import { TextField, Spinner } from "../components";
 import { SPACING, FONT_FAMILY, FONT_SIZE, COLOR } from "../theme";
 import { LOGIN_SCREEN } from "../constants";
+import { recoverPassword } from "../api";
 
 const styles = StyleSheet.create({
   container: {
@@ -28,21 +23,6 @@ const styles = StyleSheet.create({
     opacity: 0.6,
     marginBottom: SPACING.EXTRA_LARGE
   },
-  input: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    borderBottomColor: "rgba(112, 112, 112, 0.5)",
-    borderBottomWidth: 1,
-    marginBottom: SPACING.LARGE,
-    paddingBottom: Platform.OS === "ios" ? SPACING.SMALL : 0
-  },
-  textInput: {
-    flex: 1,
-    fontFamily: FONT_FAMILY.NUNITO_REGULAR,
-    fontSize: FONT_SIZE.MEDIUM,
-    color: "rgba(11, 13, 15, 0.5)"
-  },
   recoverButton: {
     height: 52,
     justifyContent: "center",
@@ -57,31 +37,99 @@ const styles = StyleSheet.create({
   }
 });
 
-export default function ForgotPassword({ navigation }) {
-  const [email, setEmail] = useState("");
+export default class ForgotPassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: { email: { value: "", error: null } },
+      loading: false
+    };
+    this.onRecoverPassword = this.onRecoverPassword.bind(this);
+  }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Forgot Password!</Text>
-      <Text style={styles.subTitle}>Please enter email to your account.</Text>
-      <View style={styles.input}>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={setEmail}
-          value={email}
-          placeholder="Email Address"
-          returnKeyType="go"
-          autoCompleteType="email"
-          keyboardType="email-address"
-          textContentType="emailAddress"
-        />
+  onRecoverPassword() {
+    const {
+      form: { email }
+    } = this.state;
+    const { navigation } = this.props;
+
+    this.setState({ loading: true });
+    recoverPassword({ email: email.value })
+      .then(() => {
+        this.setState({ loading: false });
+        Alert.alert(
+          "Recover Password",
+          "We have sent you an email with instructions to reset your password.",
+          [{ text: "OK", onPress: () => navigation.navigate(LOGIN_SCREEN) }]
+        );
+      })
+      .catch(error => {
+        if (error.code.includes("email")) {
+          this.setFieldError("email", error.nativeErrorMessage);
+          return;
+        }
+        this.setState({ loading: false });
+        Alert.alert("Recover Password Error", error.nativeErrorMessage, [
+          { text: "OK", onPress: () => {} }
+        ]);
+      });
+  }
+
+  setFieldValue(field, value) {
+    this.setState(previousState => ({
+      ...previousState,
+      form: {
+        ...previousState.form,
+        [field]: { ...previousState.form.field, value }
+      }
+    }));
+  }
+
+  setFieldError(field, error) {
+    this.setState(previousState => ({
+      ...previousState,
+      form: {
+        ...previousState.form,
+        [field]: { ...previousState.form.field, error }
+      },
+      loading: false
+    }));
+  }
+
+  render() {
+    const {
+      form: { email },
+      loading
+    } = this.state;
+
+    return loading ? (
+      <Spinner />
+    ) : (
+      <View style={styles.container}>
+        <Text style={styles.title}>Forgot Password?</Text>
+        <Text style={styles.subTitle}>Please enter email to your account.</Text>
+        <View style={styles.input}>
+          <TextField
+            onChangeText={text => this.setFieldValue("email", text)}
+            onFocus={() => this.setFieldError("email", null)}
+            onSubmitEditing={this.onRecoverPassword}
+            value={email.value}
+            error={email.error}
+            placeholder="Email Address"
+            returnKeyType="go"
+            autoCompleteType="email"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            autoCapitalize="none"
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.recoverButton}
+          onPress={this.onRecoverPassword}
+        >
+          <Text style={styles.recoverButtonText}>RECOVER PASSWORD</Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity
-        style={styles.recoverButton}
-        onPress={() => navigation.navigate(LOGIN_SCREEN)}
-      >
-        <Text style={styles.recoverButtonText}>RECOVER PASSWORD</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    );
+  }
 }
