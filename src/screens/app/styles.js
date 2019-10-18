910bb98c10ca6afc93c53c32054a974b3d5df28c
Ion Batîr <ionbatir@gmail.com>
2019-10-18 22:24:22 +0300

    Add commonStyles for screens

---
+++ src/screens/app/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { FONT_FAMILY, FONT_SIZE } from "../../theme";
+
+export default StyleSheet.create({
+  centerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    fontFamily: FONT_FAMILY.NUNITO_LIGHT_ITALIC,
+    fontSize: FONT_SIZE.LARGE
+  }
+});
