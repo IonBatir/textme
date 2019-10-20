f435bc3be0705565f1b9b93ef0095a7542d17a70
Ion Batîr <ionbatir@gmail.com>
2019-10-20 20:46:59 +0300

    Add big-avatar image

--- assets/images/index.js
+++ assets/images/index.js
@@ -4,3 +4,4 @@ import React from "react";
 import { Image } from "react-native";
 
 export const Avatar = () => <Image source={require("./avatar.png")} />;
+export const BigAvatar = () => <Image source={require("./big-avatar.png")} />;
